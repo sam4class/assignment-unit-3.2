545a934e5e21bf70a14395e081ccf03a4261b884
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-02 15:56:15 -0600

    ready for teacher's eyes

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -29,7 +29,7 @@ for (let i=2; i<11; i+=2) {
 }
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
-for (let i=5; i>0; i--) {
+for (let i=5; i>=0; i--) {
   console.log('STRETCH: countdown from 5 to 0:',i);
 }
 
